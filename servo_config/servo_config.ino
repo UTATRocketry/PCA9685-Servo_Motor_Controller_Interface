3c25221ef17bb7f7cc9ab2b42264ea955b8c6d12
Adam Wei <65464882+adamw8@users.noreply.github.com>
2021-07-08 19:49:01 -0400

    Configuration code for SERVOMIN, SERVOMAX

---
+++ servo_config/servo_config.ino
@@ -0,0 +1,68 @@
+ /*
+ * Original sourse: https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library
+ * This is the Arduino code PAC6985 16 channel servo controller
+ * watch the video for details and demo http://youtu.be/y8X9X10Tn1k
+ *  * 
+ * Written by Ahmad Nejrabi for Robojax Video channel www.Robojax.com
+ * Date: Dec 15, 2017, in Ajax, Ontario, Canada
+ * Permission granted to share this code given that this
+ * note is kept with the code.
+ * Disclaimer: this code is "AS IS" and for educational purpose only.
+ * 
+ */
+/*************************************************** 
+  This is an example for our Adafruit 16-channel PWM & Servo driver
+  Servo test - this will drive 16 servos, one after the other
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/815
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+
+// our servo # counter
+uint8_t servonum = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("16 channel Servo test!");
+
+  pwm.begin();
+  
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  //yield();
+}
+
+// the code inside loop() has been updated by Robojax
+void loop() {
+  // see 10:56 om this video https://www.youtube.com/watch?v=y8X9X10Tn1k to configure SERVOMIN and SERVOMAX
+  pwm.setPWM(0, 0, SERVOMIN );
+  delay(500);
+  pwm.setPWM(0, 0, SERVOMAX );
+  delay(500);
+
+}
